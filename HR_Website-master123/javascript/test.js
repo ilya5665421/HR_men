fdadf196cca24e934aee494a71adbfe2b4a3f89f
ilya5665421 <166956456+ilya5665421@users.noreply.github.com>
2024-04-14 09:09:24 +0300

    Add files via upload

---
+++ HR_Website-master123/javascript/test.js
@@ -0,0 +1,21 @@
+// /api/registration
+
+// fn
+// sn
+// mail
+
+let fn = ''
+
+{/* <input value={fn} typ */}
+{/* <input value={fn} typ */}
+{/* <input value={fn} typ */}
+
+const createUser = async () => {
+    await fetch('/api/registration', {
+
+    }).then(res => console.log(res))
+}
+
+// axios
+
+
